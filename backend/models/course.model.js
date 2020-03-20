d4300821d6da4fba4f74961eff23e736b49ae8ec
Jerald Siby <jeraldsiby@Jeralds-MacBook-Air.local>
2020-03-20 10:02:15 -0400

    created course.model -jerald

---
+++ backend/models/course.model.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose"); // require mongoose
+
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema( // creating new schema
+  {
+    // Properties with validations
+    courseid: { type: Number, trim: true, minlength: 3, required: true },
+    coursename: { type: String, trim: true, minlength: 3, required: true },
+    coursestatus: { type: Boolean, required: true, default: true },
+    teacherid: { type: Number, trim: true, minlength: 3, required: true } //reference id of the users in User table
+  },
+  {
+    timestamps: true // automatically manages timestamp, when created the record
+  }
+);
+
+const Course = mongoose.model("Course", courseSchema); // attaching model name with schema
+
+module.exports = Course; // exporting model
