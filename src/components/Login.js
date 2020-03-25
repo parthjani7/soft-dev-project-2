5efb6a2ae06fa13d2d55d1eea41b76d5803997a4
Parth Jani <parthjani4@gmail.com>
2020-03-25 16:51:40 -0400

    login logic added; not working fully

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,64 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+    this.checkLogin = this.checkLogin.bind(this);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({ email: e.target.value });
+  }
+  onChangePassword(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  checkLogin(e) {
+    e.preventDefault();
+
+    const loginObj = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios
+      .post("http://localhost:3000/login", loginObj)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {});
+  }
+
   render() {
     return (
       <div className="container py-5">
         <div className="row">
           <div className="col-md-8 offset-2">
-            <div class="card">
-              <div class="card-header">
+            <div className="card">
+              <div className="card-header">
                 <strong>Login Here!</strong>
               </div>
-              <div class="card-body">
-                <form>
+              <div className="card-body">
+                <form onSubmit={this.checkLogin}>
                   <div className="form-group">
-                    <label for="Email">Email address</label>
+                    <label htmlFor="Email">Email address</label>
                     <input
                       required
                       type="email"
+                      onChange={this.onChangeEmail}
+                      value={this.state.email}
                       className="form-control"
                       placeholder="Enter your e-mail address"
-                      id="Email"
+                      id="email"
                       aria-describedby="emailHelp"
                     />
                     <small id="emailHelp" className="form-text text-muted">
@@ -28,16 +66,22 @@ export default class Login extends React.Component {
                     </small>
                   </div>
                   <div className="form-group">
-                    <label for="Password">Password</label>
+                    <label htmlFor="Password">Password</label>
                     <input
                       required
+                      onChange={this.onChangePassword}
+                      value={this.state.password}
                       type="password"
                       className="form-control"
                       placeholder="Enter your password"
-                      id="Password"
+                      id="password"
                     />
                   </div>
-                  <button type="submit" className="btn btn-primary">
+                  <button
+                    type="button"
+                    onClick={this.checkLogin}
+                    className="btn btn-primary"
+                  >
                     Submit
                   </button>
                 </form>
