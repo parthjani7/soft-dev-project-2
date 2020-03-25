17eba46184b3a2b2654e4021b5cbc6a86cc9146a
Parth Jani <parthjani4@gmail.com>
2020-03-24 22:50:37 -0400

    login and signup added to login controller for authentication

---
+++ backend/controllers/login.controller.js
@@ -0,0 +1,63 @@
+let User = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY;
+const jwtExpirySeconds = 300;
+
+exports.signup = function(req, res) {
+  const payload = {
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    type: req.body.type,
+    status: req.body.status
+  };
+
+  const user = new User(payload);
+
+  bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) throw err;
+      user.password = hash;
+      user
+        .save()
+        .then(() => res.status(201))
+        .catch(err => res.status(400).json(err));
+    });
+  });
+};
+
+exports.check = function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(user => {
+      if (!user) return res.status(404);
+
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (!isMatch) return res.status(400);
+
+        jwt.sign(
+          {
+            id: user.id,
+            name: user.username
+          },
+          secretKey,
+          {
+            algorithm: "HS256",
+            jwtExpirySeconds
+          },
+          (err, token) => {
+            res.status(200).json({
+              username: user.username,
+              token: `Bearer ${token}`
+            });
+          }
+        );
+      });
+    })
+    .catch(err => res.status(400).json("Error => " + err));
+};
--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -1,17 +1,21 @@
 module.exports = function(app) {
   var user = require("../controllers/user.controller");
   var course = require("../controllers/course.controller");
+  var login = require("../controllers/login.controller");
+
+  // Auth
+  app.post("/users", login.signup);
+  app.post("/login", login.check);
 
   // Users
-  app.get("/users/", user.index);
-  app.post("/users/", user.store);
+  app.get("/users", user.index);
   app.get("/users/:id", user.show);
   app.put("/users/:id", user.update);
   app.delete("/users/:id", user.destroy);
 
   //Course
-  app.get("/courses/", course.index);
-  app.post("/courses/", course.store);
+  app.get("/courses", course.index);
+  app.post("/courses", course.store);
   app.get("/courses/:id", course.show);
   app.put("/courses/:id", course.update);
   app.delete("/courses/:id", course.destroy);
