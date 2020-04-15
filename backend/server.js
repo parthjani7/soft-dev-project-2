16a8a5bad6a170a8112a200660e1ddb641e7ce3a
Parth Jani <parthjani4@gmail.com>
2020-04-15 19:57:00 -0400

    modification for deployment

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === "development") {
 require("./routes/index.route")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build"));
   });
 }
 
